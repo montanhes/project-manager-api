a2d71b884f8c792efc0516112d0696041fdc9aa7
Ramon Carvalho <ramon@oakz.org>
2025-12-13 18:25:34 -0300

    feat: added endpoints

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/projects', [ProjectController::class, 'index']);
+    Route::post('/projects', [ProjectController::class, 'store']);
+    Route::get('/projects/{id}', [ProjectController::class, 'show']);
+
+    Route::post('/tasks', [TaskController::class, 'store']);
+    Route::patch('/tasks/{task}/toggle', [TaskController::class, 'toggle']);
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
+});
