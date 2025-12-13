9620258c4fa3406c02da1c0bcb1bc6dd0d5c223e
Ramon Carvalho <ramon@oakz.org>
2025-12-13 18:25:11 -0300

    fix: fixed User Factory and Database Seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Project::factory()
+            ->count(3)
+            ->has(Task::factory()->count(rand(20, 50)))
+            ->create();
     }
 }
